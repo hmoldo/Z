4a827c97763fbec78b819b009ce496da0980fd31
Harry Moldovanis <hmoldo@gmail.com>
2023-12-20 23:33:51 +0100

    add imports

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 //index.js
-import * as NET from "./src/NET.js";
+import * as str from "./STRING.js";
+import * as arr from "./ARRAY.js";
+import * as net from "./NET.js";
+import * as url from "./URL";
+import * as valid from "./VALID.js";
+import * as math from "./MATH.js";
+import * as io from "./IO";
+import * as img from "./IMG.js";
+import * as obj from "./OBJ.js";
+import * as mime from "./MIME.js";
 
-export default { NET };
+export default {
+  str,
+  arr,
+  net,
+  url,
+  valid,
+  math,
+  io,
+  img,
+  obj,
+  mime,
+};
