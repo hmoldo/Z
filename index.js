eb87426d3c80823ad89c066f9a718250b0e25071
Harry Moldovanis <hmoldo@gmail.com>
2023-12-20 23:40:25 +0100

    fix path

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //index.js
-import * as str from "./Z/STRING.js";
-import * as arr from "./Z/ARRAY.js";
-import * as net from "./Z/NET.js";
-import * as url from "./Z/URL";
-import * as valid from "./Z/VALID.js";
-import * as math from "./Z/MATH.js";
-import * as io from "./Z/IO";
-import * as img from "./Z/IMG.js";
-import * as obj from "./Z/OBJ.js";
-import * as mime from "./Z/MIME.js";
+import * as str from "./src/STRING.js";
+import * as arr from "./src/ARRAY.js";
+import * as net from "./src/NET.js";
+import * as url from "./src/URL";
+import * as valid from "./src/VALID.js";
+import * as math from "./src/MATH.js";
+import * as io from "./src/IO";
+import * as img from "./src/IMG.js";
+import * as obj from "./src/OBJ.js";
+import * as mime from "./src/MIME.js";
 
 export default {
   str,
